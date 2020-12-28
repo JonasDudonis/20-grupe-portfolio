0070b2a67ae43ce368531ec5b46850809d74d119
Jonas Dudonis <dudonis.j@gmail.com>
2020-12-28 12:21:37 +0200

    initial commit

---
+++ Node.js
@@ -0,0 +1,20 @@
+Node.js
+
+Browser -> Node
+
+1a) npm init
+1b) npm init -y
+
+2) npm install [package-name]
+2a) npm i [package-name]
+2b) npm i -D [package-name]
+
+3a) npm run dev
+3b) npm run test
+
+
+// Trumpiausias variantas
+
+1b) npm init -y
+2b) npm i -D [package-name]
+3a) npm run dev
